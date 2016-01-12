35aadbb3ca1a2a2ffd1575aa858d02ff4ecf2551
Kasey McCormick <mccormick.kasey@gmail.com>
2016-01-11 19:57:05 -0600

    folder structure and app.rb

---
+++ app.rb
@@ -0,0 +1,11 @@
+# debugging
+require "pry"
+# web application library and domain-specific language
+require "sinatra"
+# refresh on changes
+require "sinatra/reloader"
+# create json
+require "sinatra/json"
+
+# controllers
+require_relative"controllers/main"
