9cd8d8e9e99e7eb63d53e53016cbbba034547878
Kasey McCormick <mccormick.kasey@gmail.com>
2016-01-12 19:31:44 -0600

    needed ssl true and call .body method to show object

--- app2.rb
+++ app2.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'json'
 require 'uri'
 require 'curb'
+require 'pry'
 
 
 #
@@ -91,14 +92,15 @@ require 'curb'
 # using firespring data in their attempt
 # --------------------------------
 #
-url = "https://my.firespring.com/api/v1/websites/2546/orders/1770953"
-uri = URI.parse(url)
-request = Net::HTTP::Get.new(uri.path)
-request['Accept'] = 'application/json'
-request.basic_auth('2676d01ba72843269d41e10632509f33', '' )
-response = Net::HTTP.get(uri)
-
-puts response
+# url = "https://my.firespring.com/api/v1/websites/2546/orders/1770953"
+# uri = URI.parse(url)
+# request = Net::HTTP::Get.new(uri.path)
+# binding.pry
+# request['Accept'] = 'application/json'
+# request.basic_auth('2676d01ba72843269d41e10632509f33', '' )
+# response = Net::HTTP.get(uri)
+#
+# puts response
 
 # ---------------------------------------
 #  receiving error : {"errors":[{"type":"invalid_request_error","message":"You did not provide an API key.  You need to provide your API key as the HTTP Basic Authentication user with an empty password."}]}
@@ -109,16 +111,21 @@ puts response
 # ------------------------------
 # using my data in their answer
 # ------------------------------
-# uri = URI.parse("https://my.firespring.com/api/v1/websites/2546/orders/1770953")
-#
-# http = Net::HTTP.new(uri.host, uri.port)
-# request = Net::HTTP::Get.new(uri.request_uri)
-# request.basic_auth("2676d01ba72843269d41e10632509f33", " ")
-# response = http.request(request)
-#
-# puts response
+uri = URI.parse("https://my.firespring.com/api/v1/websites/2546/orders/1770953")
+
+http = Net::HTTP.new(uri.host, uri.port)
+http.use_ssl = true
+request = Net::HTTP::Get.new(uri.request_uri)
+
+
+request.basic_auth("2676d01ba72843269d41e10632509f33", " ")
+response = http.request(request)
+
+puts response.code
+  puts response.body
 
 # -----------------
 #  reciving error .../2.2.2/lib/ruby/2.2.0/net/protocol.rb:153:in `read_nonblock': end of file reached (EOFError)
 # --------
+	
 
